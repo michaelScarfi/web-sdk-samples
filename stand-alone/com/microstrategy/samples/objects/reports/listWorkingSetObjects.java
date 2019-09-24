7842c38c077f1350dffd0eb20916f8029b14aad6
dzumbaugh <dzumbaugh@microstrategy.com>
2019-09-24 10:18:44 -0400

    be h

--- stand-alone/com/microstrategy/samples/objects/reports/listWorkingSetObjects.java
+++ stand-alone/com/microstrategy/samples/objects/reports/listWorkingSetObjects.java
@@ -1,56 +1,55 @@
-import com.microstrategy.web.objects.SimpleList;
+package com.microstrategy.samples.objects.reports;
+
+import com.microstrategy.samples.sessions.SessionManager;
 import com.microstrategy.web.objects.WebFolder;
 import com.microstrategy.web.objects.WebIServerSession;
 import com.microstrategy.web.objects.WebObjectInfo;
 import com.microstrategy.web.objects.WebObjectSource;
 import com.microstrategy.web.objects.WebObjectsException;
-import com.microstrategy.web.objects.WebObjectsFactory;
 import com.microstrategy.web.objects.WebReportInstance;
 import com.microstrategy.web.objects.WebReportSource;
 import com.microstrategy.web.objects.WebWorkingSet;
 import com.microstrategy.webapi.EnumDSSXMLObjectTypes;
 
-public class searchReportWSObjects {
+public class listWorkingSetObjects {
 
 	public static void main(String[] args) {
-		
-		// Create an IServer session
-		WebObjectsFactory objectfactory = WebObjectsFactory.getInstance();
-		WebIServerSession iServerSession = objectfactory.getIServerSession();
-	    
+
 		// Set server connection information
-	    iServerSession.setServerName("servername.microstrategy.com"); 
-	    iServerSession.setServerPort(0);
-	    iServerSession.setProjectName("MicroStrategy Tutorial"); 
-	    iServerSession.setLogin("administrator");
-	    iServerSession.setPassword("password");
-	    
-	    String reportID = "018FD577479CF15774B163B9FDF477FA";
-		
+		String intelligenceServerName = "sup-w-001603.labs.microstrategy.com"; 
+		String projectName = "MicroStrategy Tutorial"; 
+		String microstrategyUsername =  "administrator";
+		String microstrategyPassword = "";
+
+		// Create an IServer session
+		WebIServerSession iServerSession = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
+
+		// Report which will be used
+		String reportID = "018FD577479CF15774B163B9FDF477FA";
+
 		try {
 			// Call our function which will get all objects available on the working set of a report
+			// Takes the IServer session and report ID
 			getWSObjects(iServerSession, reportID);
 		} catch (WebObjectsException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	// getWSObjects is used to gather all objects found on the report's working set. 
-	// Requires an active IServer session as well as a given report ID
+
 	private static void getWSObjects(WebIServerSession session, String reportID) throws WebObjectsException {
 		//Create a report source with the server session
 		WebReportSource reportSource = session.getFactory().getReportSource();
-		
+
 		// Get a report instance given a reportID
 		WebReportInstance reportInstance = reportSource.getNewInstance(reportID);
-		
+
 		// Get the working set instance for this report
 		WebWorkingSet reportWS = reportInstance.getWorkingSet();
-		
+
 		// Get all working set objects within a Web Folder object
 		WebFolder wsFolderObjects = reportWS.getWorkingSetObjects();
-		
+
 		// Loop through the Web Folder and output the display name for each object
 		WebObjectInfo objectInfo = null;
 		if(wsFolderObjects.size() > 0) {
@@ -60,5 +59,5 @@ public class searchReportWSObjects {
 			}
 		}
 	}
-	
+
 }
