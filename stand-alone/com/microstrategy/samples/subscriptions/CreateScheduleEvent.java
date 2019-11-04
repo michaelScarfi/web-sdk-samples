fec0a92554dac30e52f3fc5a83bb847440f4f43c
tiagosiebler <tiagosiebler@github.com>
2019-11-04 15:22:59 +0000

    feat: add simple sample for creating a schedule event

---
+++ stand-alone/com/microstrategy/samples/subscriptions/CreateScheduleEvent.java
@@ -0,0 +1,45 @@
+package com.microstrategy.samples.subscriptions;
+
+import com.microstrategy.samples.sessions.SessionManager;
+import com.microstrategy.utils.Tree;
+import com.microstrategy.web.objects.*;
+import com.microstrategy.webapi.*;
+
+public class CreateScheduleEvent {
+
+    public static void main(String[] args) {
+        // A schedule is a configuration-level object (not project level). Thus the session must be without a project too.
+        String intelligenceServerName = "10.27.72.72";
+        String projectName = "";
+        String microstrategyUsername = "administrator";
+        String microstrategyPassword = "";
+        
+        // Establishes a connection to the Intelligence Server
+        WebIServerSession serverSession = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
+        
+        try {
+            saveNewScheduleEvent(serverSession);
+        } catch (WebObjectsException | IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    // Create a new event and save it in the events folder 
+    // In Developer, find this in Administration -> Configuration Managers -> Events
+    public static void saveNewScheduleEvent(WebIServerSession serverSession) throws WebObjectsException, IllegalArgumentException {
+        WebObjectsFactory factory = serverSession.getFactory();
+        WebObjectSource wos = factory.getObjectSource();
+        
+        String eventsFolderId = wos.getFolderID(EnumDSSXMLFolderNames.DssXmlFolderNameEvents);
+        WebFolder eventsFolder = (WebFolder) wos.getObject(eventsFolderId, EnumDSSXMLObjectTypes.DssXmlTypeFolder, true);
+
+        WebScheduleEvent eventSchedule = (WebScheduleEvent) wos.getNewObject(EnumDSSXMLObjectTypes.DssXmlTypeScheduleEvent);
+        eventSchedule.setDescription("This is a new event created using the Web SDK");
+        
+        wos.saveAs(eventSchedule, "My New Event Name", eventsFolder, true);
+        
+        System.out.println("New event saved with ID: " + eventSchedule.getID());
+    }
+
+}
