7facead2a19274b774a7a217476af9e758bb8bc6
michaelScarfi <michael.scarfi@gmail.com>
2020-05-29 14:14:52 -0400

    Update ObjectSearchByName.java
    Fixed indentation issues

--- stand-alone/com/microstrategy/samples/searching/ObjectSearchByName.java
+++ stand-alone/com/microstrategy/samples/searching/ObjectSearchByName.java
@@ -21,17 +21,17 @@ import com.microstrategy.web.objects.*;
 public class ObjectSearchByName {
 
 	public static void main(String[] args) {
-		// Connectivity for the intelligence server
+	    // Connectivity for the intelligence server
 	    final String intelligenceServerName = "sup-w-001643.labs.microstrategy.com";
 	    final String projectName = "MicroStrategy Tutorial";
 	    final String microstrategyUsername = "Administrator";
 	    final String microstrategyPassword = "";
 	    
 	    /**
-		 * This determines the type of object for the search, currently it is set to attribute
-		 * Changing DSSXML will allow you to choose what type of object the search will return
-		 * Values for this can be found at https://lw.microstrategy.com/msdz/MSDL/GARelease_Current/docs/ReferenceFiles/reference/com/microstrategy/webapi/EnumDSSXMLObjectSubTypes.html
-		 */
+	     * This determines the type of object for the search, currently it is set to attribute
+	     * Changing DSSXML will allow you to choose what type of object the search will return
+	     * Values for this can be found at https://lw.microstrategy.com/msdz/MSDL/GARelease_Current/docs/ReferenceFiles/reference/com/microstrategy/webapi/EnumDSSXMLObjectSubTypes.html
+	     */
 	    final int objectType = EnumDSSXMLObjectSubTypes.DssXmlSubTypeAttribute;
 	    
 	    // Object Name for search
