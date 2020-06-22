22d5f1e71809f169b429a3b7dbc19b45c63fb96f
michaelScarfi <michael.scarfi@gmail.com>
2020-06-22 12:42:11 -0400

    Update ObjectSearchByName.java
    Updated to return object instead of String ID

--- stand-alone/com/microstrategy/samples/searching/ObjectSearchByName.java
+++ stand-alone/com/microstrategy/samples/searching/ObjectSearchByName.java
@@ -5,7 +5,7 @@ package com.microstrategy.samples.searching;
  * Purpose: to find a user's ID using their name
  * 
  * Notes: The method searchObject can be used as a standalone function
- * The searchObject method will only search for one type of object as defined by int objectType (see notes for values)
+ * The searchObject method will only search for one type of object as defined by int OBJECT_TYPE (see notes for values)
  * 
  * Created by: Michael Scarfi
  * Creation date: 5/29/2020
@@ -21,44 +21,49 @@ import com.microstrategy.web.objects.*;
 public class ObjectSearchByName {
 
 	public static void main(String[] args) {
-	    // Connectivity for the intelligence server
-	    final String intelligenceServerName = "sup-w-001643.labs.microstrategy.com";
-	    final String projectName = "MicroStrategy Tutorial";
-	    final String microstrategyUsername = "Administrator";
-	    final String microstrategyPassword = "";
+		// Connectivity for the intelligence server
+	    final String INTELLIGENCE_SERVER_NAME = "sup-w-001643.labs.microstrategy.com";
+	    final String PROJECT_NAME = "MicroStrategy Tutorial";
+	    final String MICROSTRATEGY_USERNAME = "Administrator";
+	    final String MICROSTRATEGY_PASSWORD = "";
 	    
 	    /**
-	     * This determines the type of object for the search, currently it is set to attribute
-	     * Changing DSSXML will allow you to choose what type of object the search will return
-	     * Values for this can be found at https://lw.microstrategy.com/msdz/MSDL/GARelease_Current/docs/ReferenceFiles/reference/com/microstrategy/webapi/EnumDSSXMLObjectSubTypes.html
-	     */
-	    final int objectType = EnumDSSXMLObjectSubTypes.DssXmlSubTypeAttribute;
+		 * This determines the type of object for the search, currently it is set to attribute
+		 * Changing DSSXML will allow you to choose what type of object the search will return
+		 * Values for this can be found at https://lw.microstrategy.com/msdz/MSDL/GARelease_Current/docs/ReferenceFiles/reference/com/microstrategy/webapi/EnumDSSXMLObjectSubTypes.html
+		 */
+	    final int OBJECT_TYPE = EnumDSSXMLObjectSubTypes.DssXmlSubTypeAttribute;
 	    
 	    // Object Name for search
 	    String searchName = "test";
 	    
 	    // Create our I-Server Session
-	    WebIServerSession session = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
+	    WebIServerSession session = SessionManager.getSessionWithDetails(INTELLIGENCE_SERVER_NAME, PROJECT_NAME, MICROSTRATEGY_USERNAME, MICROSTRATEGY_PASSWORD);
 	    
 	    // Find User and output search results
-	    String[] objectIDs = searchObject(session, searchName, objectType);
+	    WebObjectInfo[] objects = searchObject(session, searchName, OBJECT_TYPE);
 	    
-	    // As multiple objects can have the same name, this iterates through returned values and displays each
-	    for (int i = 0; i < objectIDs.length; i++) {
-	    	System.out.println("Object ID: " + objectIDs[i]);
+	    if(objects == null) {
+	    	System.out.println("No objects found.");
+	    }else {
+	    	// As multiple objects can have the same name, this iterates through returned values and displays each
+		    for (int i = 0; i < objects.length; i++) {
+		    	System.out.println("Object ID: " + objects[i].getID());
+		    }
 	    }
+	    
 	    
 	}
 	 
 	/**
-	 * Takes a WebIServerSession object, and a String name and searches for the IDs of objects that matche the name provided
+	 * Takes a WebIServerSession object, and a String name and searches for the IDs of objects that matches the name provided
 	 * 
 	 * @param session Object of type WebIServerSession
 	 * @param searchName String containing the search String
 	 * @param objectType int containing the int value of the object type for the search
-	 * @return String[] containing the object IDs, if none are found first string value will equal "no objects found"
+	 * @return WebObjectInfo[] containing the objects, if none are found the string will be of value null
 	 */
-	private static String[] searchObject(WebIServerSession session, String searchName, int objectType) {
+	private static WebObjectInfo[] searchObject(WebIServerSession session, String searchName, int objectType) {
 		//Create and populate Factory
 		WebObjectSource webObjectSource = session.getFactory().getObjectSource();
 		
@@ -67,7 +72,7 @@ public class ObjectSearchByName {
 		WebSearch search = webObjectSource.getNewSearchObject();
 		
 		//Create empty String to store search results
-		String[] objectIDs = {"no objects found"};
+		WebObjectInfo[] objectIDs = null;
 		
 		try {
 			//Populate search parameters
@@ -92,20 +97,20 @@ public class ObjectSearchByName {
 
 			//iterate through search results and add to objectIDs
 			if (size > 0) {
-				System.out.println("found something");
-				objectIDs = new String[size];
+				objectIDs = new WebObjectInfo[size];
 				for(int i = 0; i < f.size();i++) {
 					object = (WebObjectInfo) f.get(i);
 					object.populate();
-					objectIDs[i] = object.getID();
+					objectIDs[i] = object;
 				}
+				return objectIDs;
 			}
 			
 		} catch (Exception e) {
 			//print error message to console if an exception occurs.
 			System.out.println(e.getMessage());
 		}
-		
+		//returning null object if no values found
 		return objectIDs;
 		
 	}
