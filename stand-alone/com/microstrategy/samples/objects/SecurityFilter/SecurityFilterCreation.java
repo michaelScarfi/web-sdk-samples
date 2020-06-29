1a5060018a4198100c58a955538a1fdf035642e6
michaelScarfi <michael.scarfi@gmail.com>
2020-06-29 18:17:30 -0400

    Changed class name and created method

--- stand-alone/com/microstrategy/samples/objects/SecurityFilter/SecurityFilterCreation.java
+++ stand-alone/com/microstrategy/samples/objects/SecurityFilter/SecurityFilterCreation.java
@@ -1,4 +1,4 @@
-package com.microstrategy.samples.objects.WebExpression;
+package com.microstrategy.samples.objects.SecurityFilter;
 
 import com.microstrategy.samples.sessions.SessionManager;
 import com.microstrategy.web.objects.WebExpression;
@@ -10,39 +10,39 @@ import com.microstrategy.web.objects.admin.users.WebMDSecurityFilter;
 import com.microstrategy.webapi.EnumDSSXMLObjectTypes;
 import com.microstrategy.webapi.EnumDSSXMLFolderNames;
 
-public class Create_Web_Expression {
+public class SecurityFilterCreation {
 
 	public static void main(String[] args) {	
 		// Connectivity for the intelligence server
 		String intelligenceServerName = "sup-w-001643.labs.microstrategy.com";
 	    String projectName = "MicroStrategy Tutorial";
 	    String microstrategyUsername = "Administrator";
-	    String microstrategyPassword = "";
-        String value = "23";
+		String microstrategyPassword = "";
         String filterName = "test filter";
-			    
+		String expression = "[Employee Age]@ID=23";
+
 	    // Create our I-Server Session
 	    WebIServerSession session = SessionManager.getSessionWithDetails(intelligenceServerName, projectName, microstrategyUsername, microstrategyPassword);
-	    WebObjectSource webObjectSource = session.getFactory().getObjectSource();
 	    
-	    WebMDSecurityFilter wkFilter = (WebMDSecurityFilter) webObjectSource.getNewObject( EnumDSSXMLObjectTypes.DssXmlTypeMDSecurityFilter );
-        WebExpression wkExpr = wkFilter.getExpression();
-        
-        
+		CreateSecurityFilter(session, expression, filterName);
+	}
 
-        try {
+	public static void CreateSecurityFilter(WebIServerSession session, String expression, String filterName){
+		WebObjectSource webObjectSource = session.getFactory().getObjectSource();
+	    
+	    WebMDSecurityFilter wMDFilter = (WebMDSecurityFilter) webObjectSource.getNewObject( EnumDSSXMLObjectTypes.DssXmlTypeMDSecurityFilter );
+		WebExpression wExpr = wMDFilter.getExpression();
+		
+		try {
             String folderID = webObjectSource.getFolderID(EnumDSSXMLFolderNames.DssXmlFolderNameSystemMDSecurityFilters);
             WebFolder folder = (WebFolder) webObjectSource.getObject(folderID, EnumDSSXMLObjectTypes.DssXmlTypeFolder);
 
-            wkExpr.populate( "[Employee Age]@ID=" + value);
-            webObjectSource.save(wkFilter, filterName, folder);
+            wExpr.populate(expression);
+            webObjectSource.save(wMDFilter, filterName, folder);
 		} catch (WebObjectsException e) {
 			e.printStackTrace();
 		} 
-        
-        
-	    
-	    
+
 	}
 
 }
